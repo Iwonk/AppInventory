56fa1863446835d979e21a68424dbee1498ec8a5
iwonk <i.wonk100@gmail.com>
2019-11-14 17:08:29 +0700

    kwowkokw

---
+++ app/src/main/java/com/bluohazard/appinventory/CreateData.java
@@ -0,0 +1,77 @@
+package com.bluohazard.appinventory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bluohazard.appinventory.models.Barang;
+
+public class CreateData extends Activity implements View.OnClickListener {
+
+    // inisialisasi elemen-elemen pada layout
+    private Button buttonSubmit;
+    private EditText edNama;
+    private EditText edMerk;
+    private EditText edHarga;
+
+    // inisialisasi kontroller/Data Source
+    private DBDataSource dataSource;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_data);
+
+        buttonSubmit = findViewById(R.id.button_submit);
+        buttonSubmit.setOnClickListener(this);
+        edNama = findViewById(R.id.nama_barang);
+        edHarga = findViewById(R.id.harga_barang);
+        edMerk = findViewById(R.id.merk_barang);
+
+        // instanstiasi kelas DBDataSource
+        dataSource = new DBDataSource(this);
+
+        // membuat sambungan baru ke database
+        dataSource.open();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Inisialisasi data barang
+        String nama = null;
+        String merk = null;
+        String harga = null;
+        @SuppressWarnings("unused")
+
+        // inisialisasi barang baru (masih kosong)
+                Barang barang = null;
+        if (edNama.getText() != null && edMerk.getText() != null && edHarga.getText() != null) {
+            // Jika field nama, merk, dan harga tidak kosong maka masukkan ke dalam data barang
+            nama = edNama.getText().toString();
+            merk = edMerk.getText().toString();
+            harga = edHarga.getText().toString();
+        }
+
+        switch (v.getId()) {
+            case R.id.button_submit:
+                // insert data barang baru
+                barang = dataSource.createBarang(nama, merk, harga);
+
+                // konfirmasi kesuksesan
+                Toast.makeText(this, "masuk Barang\n" +
+                        "nama" + barang.getNama_barang() +
+                        "merk" + barang.getMerk_barang() +
+                        "harga" + barang.getHarga_barang(), Toast.LENGTH_LONG
+                ).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
